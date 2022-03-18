8811b902f9270b5d765ca72fc0e5da5210729a2d
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2022-03-17 17:58:57 -0700

    About Me skeleton added

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ function IndexPage() {
 		<div className="page-style">
 			{/* <title>Taryn Wou</title> */}
 			<div class="container">
+				{/* // HOME PAGE */}
 				<div class="row">
 					<Navigation></Navigation>
 				</div>
@@ -50,9 +51,47 @@ function IndexPage() {
 				<div class="row">
 					<img className="arrow" src={Arrow} alt="Down Arrow" />
 				</div>
+				{/* // ABOUT ME */}
+				<div class="row">
+					<div class="col-lg-1"></div>
+					<div class="col-lg-6">
+						<h1 className="title-font">
+							About Me
+							<br />
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua.
+							<br></br>
+							Ut enim ad minim veniam, quis nostrud exercitation
+							ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in
+							voluptate velit esse cillum dolore eu fugiat nulla
+							pariatur.
+							<br></br>
+							Excepteur sint occaecat cupidatat non proident, sunt
+							in culpa qui officia deserunt mollit anim id est
+							laborum.
+						</p>
+					</div>
+					<div class="col-lg-4">
+						<img
+							className="headshot"
+							src={Headshot}
+							alt="Taryn Wou Headshot"
+						/>
+					</div>
+					<div class="col-lg-1"></div>
+				</div>
+				<div class="row">
+					<p className="next-sect-link">check out my work</p>
+				</div>
+				<div class="row">
+					<img className="arrow" src={Arrow} alt="Down Arrow" />
+				</div>
 			</div>
 		</div>
-		// </main>
 	);
 }
 
