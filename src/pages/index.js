7ee38596350db642db75a254ccd2ae4ae183f909
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2022-04-03 20:16:04 -0700

    Added About Me content

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,36 +53,52 @@ function IndexPage() {
 				</div>
 				{/* // ABOUT ME */}
 				<div class="row">
-					<div class="col-lg-1"></div>
-					<div class="col-lg-6">
-						<h1 className="title-font">
+					{/* <div class="col-lg-1"></div> */}
+					<div class="col-lg-8">
+						<h1 className="title-font" id="title-about-me">
 							About Me
 							<br />
 						</h1>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit, sed do eiusmod tempor incididunt ut labore et
-							dolore magna aliqua.
+						<p id="about-me-blurb">
+							Hi there! My name is Taryn, and as a lifelong
+							learner, I have always tried to put myself in
+							situations that allow me to meet new people and grow
+							as an individual. Currently, I am interested in
+							pursuing front-end development and UI/UX. Though, I
+							am an explorer of many interests, including social
+							entrepreneurship, marketing, digital media,
+							psychology, and education.
 							<br></br>
-							Ut enim ad minim veniam, quis nostrud exercitation
-							ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla
-							pariatur.
 							<br></br>
-							Excepteur sint occaecat cupidatat non proident, sunt
-							in culpa qui officia deserunt mollit anim id est
-							laborum.
+							Service is a large part of my everyday being, and I
+							hope to drive value back into the communities that
+							have touched my life. I would like to think all of
+							the activities I partake in, be it work or
+							extracurricular-related, have a positive impact on
+							the community in one way or another. At my core, I
+							am extremely passionate about making education and
+							personal development opportunities more accessible,
+							especially to underserved demographics.
+							<br></br>
+							<br></br>
+							In my spare time, you may find me playing board
+							games with friends, listening to a random (but
+							informative) podcast, or chowing down on a bowl of
+							poké. I am an extroverted introvert who draws energy
+							from constantly being around others but also stays
+							up late to be alone with their thoughts. All in all,
+							I am curious about new experiences and welcome
+							anyone who wants to chat!
 						</p>
 					</div>
 					<div class="col-lg-4">
 						<img
-							className="headshot"
+							className="headshot-2"
 							src={Headshot}
 							alt="Taryn Wou Headshot"
 						/>
 					</div>
-					<div class="col-lg-1"></div>
+					{/* <div class="col-lg-1"></div> */}
 				</div>
 				<div class="row">
 					<p className="next-sect-link">check out my work</p>
